3b858cb9ed89c62aa00566947559d93cad01458b
JamesCanterbury <james.canterbury@gmail.com>
2017-09-04 10:01:21 -0400

    new commit

---
+++ extract.R
@@ -0,0 +1,11 @@
+# Extract entities from an AnnotatedPlainTextDocument
+entities <- function(doc, kind) {
+  s <- doc$content
+  a <- annotations(doc)[[1]]
+  if(hasArg(kind)) {
+    k <- sapply(a$features, `[[`, "kind")
+    s[a[k == kind]]
+  } else {
+    s[a[a$type == "entity"]]
+  }
+}
--- script_processing.R
+++ script_processing.R
@@ -1,4 +1,4 @@
-require(tidyverse)
+require(tidyverse)si
 require(rJava)
 require(NLP)
 require(NLP)
@@ -11,5 +11,13 @@ require(qdap)
 word_ann <- Maxent_Word_Token_Annotator()
 sent_ann <- Maxent_Sent_Token_Annotator()
 
-sith_annotations <- annotate(sith_script_lines, list(sent_ann, word_ann))
-sith_doc <- 
+##sith_annotations <- annotate(sith_script_lines, list(sent_ann, word_ann))
+##sith_doc <-  AnnotatedPlainTextDocument(sith_script_lines, sith_annotations)
+
+person_ann <- Maxent_Entity_Annotator(kind = "person")
+location_ann <- Maxent_Entity_Annotator(kind = "location")
+organization_ann <- Maxent_Entity_Annotator(kind = "organization")
+pipeline <- list(sent_ann,word_ann, person_ann, location_ann, organization_ann)
+
+sith_annotations <- annotate(sith_script_lines, pipeline)
+sith_doc <- AnnotatedPlainTextDocument(sith_script_lines, sith_annotations)
